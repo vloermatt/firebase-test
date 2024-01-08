e1b222bd7ba03c8646aa2c2a45fff56821c1c09f
vloermatt <mdldavies@gmail.com>
2024-01-08 14:30:57 +0200

    est

--- set-envs.sh
+++ set-envs.sh
@@ -16,12 +16,12 @@ for secret in "${env_list[@]}"; do
     if echo "$output" | grep -q "$secret"; then
         # Secret is already present, update version
         echo "updating $secret version..."
-        echo -n "${{ secrets."$secret" }}" | gcloud secrets versions add $secret \
+        echo -n "${{ secrets[$secret] }}" | gcloud secrets versions add $secret \
                 --data-file=-
     else
         # Secret does not exist, create first version
         echo "creating $secret..."
-        echo -n "${{ secrets."$secret" }}" | gcloud secrets create $secret \
+        echo -n "${{ secrets[$secret] }}" | gcloud secrets create $secret \
                 --replication-policy="automatic" \
                 --data-file=-
     fi
